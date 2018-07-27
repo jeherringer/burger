3f646584292506428fd8536d860deaf3b15d4d3c
jeherringer <j.e.herringer@gmail.com>
2018-07-26 23:57:20 -0500

    added JawsDB

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
-var express = require("express")
+var express = require("express");
 var bodyParser = require("body-parser");
+
+var app = express();
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+app.use(methodOverride("_method"));
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+	defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+app.use(express.static('./public'));
+ 
+require("./controllers/burger_controller.js")(app);
+
+app.set('port', process.env.PORT);
+
+app.listen(app.get('port'), () => {
+    console.log(`Express app listening on ${app.get('port')}`);
+});
